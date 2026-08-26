416c7c3f698b8f9a52f4bf3cbca701faa49197e0
Letsuan <pdi.taiwan@gmail.com>
2026-08-26 21:38:42 +0800

    Update fetch path for index file

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
           const url = new URL(request.url);
           if (url.pathname === '/' || url.pathname === '/index.html') {
-                  url.pathname = '/index.dc.html';
+                  url.pathname = '/index.dc';
                   return env.ASSETS.fetch(new Request(url, request));
           }
           return env.ASSETS.fetch(request);
